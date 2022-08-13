0a35038c7d99408225264c5316d5508e4fa58a1b
Bruno <alvesvaleriobruno@gmail.com>
2022-08-13 17:32:07 -0300

    Atualizando

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,12 +1,13 @@
 class Negociacao {
     constructor(data, quantidade, valor){
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     get Data(){
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get Quantidade(){
