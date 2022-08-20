269d522785cc7699211deac691203274ce96e035
Bruno <alvesvaleriobruno@gmail.com>
2022-08-20 16:02:42 -0300

    Atualizando datas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,7 +13,17 @@ class NegociacaoController{
 
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
-        console.log(data);
+        let data = new Date(...this._inputData.value.split('-').map((item, indice) => item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        let diaMesAno = negociacao.data.getDate() + '/' + (negociacao.data.getMonth() + 1) + '/' + negociacao.data.getFullYear();
+
+        console.log(diaMesAno);
     }
 }
